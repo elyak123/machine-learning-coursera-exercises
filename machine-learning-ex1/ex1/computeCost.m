45d33c9e0d45fca9f7b4c596b4db37a907d3a9b3
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-07-09 10:47:36 -0500

    cost function funcionanado

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -1,25 +1,24 @@
 function J = computeCost(X, y, theta)
-%COMPUTECOST Compute cost for linear regression
-%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
-%   parameter for linear regression to fit the data points in X and y
+	%COMPUTECOST Compute cost for linear regression
+	%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+	%   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
+	% Initialize some useful values
+	m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+	% You need to return the following variables correctly 
+	J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-indices = 0;
-for it = 1:m
-	temp = (theta(1) + theta(2)*X(it) - y(it))^2;
-	indices = indices + temp;
-end
+	% ====================== YOUR CODE HERE ======================
+	% Instructions: Compute the cost of a particular choice of theta
+	%               You should set J to the cost.
+	indices = 0;
+	for it = 1:m
+		indices = indices + (theta' * X(it,:)' - y(it))^2;
+	end
 
-J = indices / (2*m);
+	J = indices / (2*m);
 
-% =========================================================================
+	% =========================================================================
 
 end
